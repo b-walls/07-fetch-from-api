b908180f0752b48a7a511620c48a4abe8d13e8a8
Brendan Walls <wallsbr@dukes.jmu.edu>
2025-07-17 20:17:32 +0000

    Add joke fetching functionality on button click

--- script.js
+++ script.js
@@ -6,4 +6,11 @@ const punchline = document.getElementById('punchline');
 // Add an event listener to the button
 jokeBtn.addEventListener('click', function() {
   console.log('Getting a joke...');
+  fetch('https://official-joke-api.appspot.com/random_joke')
+    .then(response => response.json())
+    .then(data => {
+      console.log('Joke received:', data);
+      setup.textContent = data.setup;
+      punchline.textContent = data.punchline;
+    });
 });
